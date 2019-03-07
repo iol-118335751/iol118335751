f4ff6ed9fe19e5b26f34b8b719b02f4ac0f8393f
118335751 <118335751@BISB27VL.bis.ad.ucc.ie>
2019-03-07 14:52:09 +0000

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title><a href="
-          https://git.heroku.com/iol-phpproject10.git">118335751- Homepage</a></title>
+<title><a href="https://iol118335751.herokuapp.com/">118335751- Homepage</a></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <LINK rel="stylesheet" type="text/css" href="css/index.css" />
 
 </head>
 <body>
- 
+    <h2><a href="https://iol118335751.herokuapp.com/">118335751- Homepage</a></h2>
+    <br />
 <div class="row">
   <div class="column1" ><a href="cv.html">Curriculum Vitae</a></div>
   <div class="column2" ><a href="interestshtml.html">Interests</a></div>
-  <div class="column3" ><a href="consulting.html">Consulting Services</a></div>
+  <div class="column3" ><a href="consulthome.html">Consulting Services</a></div>
 </div>
 </body>
 </html>
